05b8239ee9fdefaf80cc0165f10a15074439bcba
iceedoc <paashtetik@gmail.com>
2023-12-15 11:11:38 +0300

    variables

---
+++ src/Main.java
@@ -0,0 +1,91 @@
+public class Main {
+    public static void main(String[] args) {
+      int apples = 150;
+      byte table = 10;
+      short potatoes = 500;
+      long berries = 10000L;
+      float water = 0.75f;
+      double oxygen = 0.505;
+      System.out.println(apples);
+      System.out.println(table);
+      System.out.println(potatoes);
+      System.out.println(berries);
+      System.out.println(water);
+      System.out.println(oxygen);
+      double a = 27.12;
+      long b = 987678965549L;
+      float c = 2.786f;
+      short d = 569;
+      int e = -159;
+      long h = 27897L;
+      byte r = 67;
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+      System.out.println(d);
+      System.out.println(e);
+      System.out.println(h);
+      System.out.println(r);
+      byte lyudmila = 23;
+      byte anna = 27;
+      byte catherine = 30;
+      int paper = 480;
+      System.out.println(paper / (lyudmila + anna + catherine));
+      byte oneMinute = 16 / 2;
+      System.out.println(oneMinute);
+      byte twentyMinutes = 20;
+      int day = 1444;
+      short threeDays = 4320;
+      long month = 43200;
+      System.out.println(twentyMinutes * oneMinute);
+      System.out.println(day * oneMinute);
+      System.out.println(threeDays * oneMinute);
+      System.out.println(month * oneMinute);
+      int totalCans = 120;
+      byte oneClass = 2 + 4;
+      System.out.println(oneClass);
+      int totalClasses = totalCans / oneClass;
+      System.out.println(totalClasses);
+      int white = totalClasses * 2;
+      int brown = totalClasses * 4;
+      System.out.println(white);
+      System.out.println(brown);
+      int bananas = 5 * 80;
+      int milk = (200 /100) * 105;
+      int iceCreamSundae = 2 * 100;
+      int eggs = 4 * 70;
+      System.out.println(bananas);
+      System.out.println(milk);
+      System.out.println(iceCreamSundae);
+      System.out.println(eggs);
+      int oneKilogram = 1000;
+      int onlyGram = bananas + milk + iceCreamSundae + eggs;
+      System.out.println(oneKilogram);
+      System.out.println(onlyGram);
+      double inKilogram = (double)onlyGram / oneKilogram;
+      System.out.println(inKilogram);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
